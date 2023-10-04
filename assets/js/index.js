9838b5f28f0d72de4a4afbcbcff99abddae6de6b
DESKTOP-IS2G159\kevin <kevin.rodfa@gmail.com>
2023-10-04 00:54:35 -0300

    segunda parte del desafio 2 terminada

--- assets/js/index.js
+++ assets/js/index.js
@@ -24,19 +24,53 @@ imagen.addEventListener("click", cambiaColor);
 
 //Desafio 2 parte 2
 
-let botonVerificar = document.querySelector("botonVerificar")
+let botonVerificar = document.querySelector(".botonVerificar");
+let cantidadStickers = document.querySelector(".cantidadStickers");
+
 
 function capturarValores(){
 
     let valorUno = document.getElementById("valorUno").value;
     let valorDos = document.getElementById("valorDos").value;
     let valorTres = document.getElementById("valorTres").value;
+    let totalStickers = parseInt(valorUno) + parseInt(valorDos) + parseInt(valorTres);
 
-    console.log(valorUno+valorDos+valorTres);
-   
+    if(totalStickers<=10){
+        cantidadStickers.innerHTML = "Llevas " + totalStickers + " stickers";
+    }
+
+    else{
+
+        cantidadStickers.innerHTML = "Llevas demasiados stickers";
+    }
+
 }
 
+/* function handleValue(e){
+
+    let value =e.target.value;
+    if (value > 10){
+
+        e.target.value=10;
+    }
+
+    else if (value <0){
+
+        e.target.value=0;
+    }  
+    
+}
+
+document.getElementById("valorUno").addEventListener("change", handleValue)
+ */
 botonVerificar.addEventListener("click", capturarValores);
+
+//desafio 2 parte 3 
+
+
+
+
+
 
 
 
