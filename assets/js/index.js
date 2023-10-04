c22aff0cec429f1ba392a6e5fb2d7b4971c2ea7b
DESKTOP-IS2G159\kevin <kevin.rodfa@gmail.com>
2023-10-04 02:00:48 -0300

    trabajo terminado, faltan posibles detalles

--- assets/js/index.js
+++ assets/js/index.js
@@ -67,6 +67,38 @@ botonVerificar.addEventListener("click", capturarValores);
 
 //desafio 2 parte 3 
 
+let buttonIngresar = document.querySelector(".buttonIngresar");
+let respuestaPassword = document.querySelector(".respuestaPassword");
+
+function verificaPassword(){
+
+    let primerNumero = document.querySelector(".primerNumero").value;
+    let segundoNumero = document.querySelector(".segundoNumero").value;
+    let tercerNumero = document.querySelector(".tercerNumero").value;
+    let password = primerNumero + segundoNumero + tercerNumero;
+
+    if(password==911){
+
+        respuestaPassword.innerHTML = "Password 1 correcto"
+
+    }
+
+    else if(password==714){
+
+        respuestaPassword.innerHTML = "Password 2 correcto"
+    }
+
+    else{
+
+        respuestaPassword.innerHTML = "Password incorrecto"
+    }
+    
+    
+
+}
+
+buttonIngresar.addEventListener("click", verificaPassword);
+
 
 
 
